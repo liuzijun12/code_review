6350b080323c171f9b0b2a0923660e6cea74ae01
liuzijun <18330177876@163.com>
2025-09-21 13:22:09 +0800

    完善ALLOW_HOSTS从环境变量中获得

--- code_review/settings.py
+++ code_review/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-2k=x9_t=1)sw*@#oabqe0*r34t*%goq95o^)$0*l_90rv4cb4*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['976daab5f06f.ngrok-free.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
